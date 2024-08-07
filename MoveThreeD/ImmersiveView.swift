7c02c5824126552c8e68e6dec9112ed84384b21d
Subash Shrestha <119420796+arconsis-subash@users.noreply.github.com>
2024-08-07 14:54:32 +0200

    update: added space skybox and added models

--- MoveThreeD/ImmersiveView.swift
+++ MoveThreeD/ImmersiveView.swift
@@ -8,45 +8,33 @@
 import SwiftUI
 import RealityKit
 import RealityKitContent
+import UIKit
 
 struct ImmersiveView: View {
+    
     var body: some View {
         RealityView { content in
             if let skyBox  = createSkyBox(){
                 content.add(skyBox)
             }
-            
-            // Earth model
-            
-            if let earthModel  = try? await Entity(named: "Earth"),
-               
-               let earth =  earthModel.children.first?.children.first,
-               let environment =  try? await EnvironmentResource(named: "ImageBasedLight"){
-                     earth.position.y = 0.5
-                     earth.position.x = 1
+            if let earthModel =  buildModel( modelName:"Earth", scaleValue: 0.5){
                 
-                     earth.scale = [0.5,0.5,0.5]
-                     
-                     // Generate the collision effect with the base
-                     earth.generateCollisionShapes(recursive: true)
-                     earth.components.set(InputTargetComponent(allowedInputTypes: .all))
-                     earth.components.set(ImageBasedLightComponent(source: .single(environment)))
-                     earth.components.set(ImageBasedLightReceiverComponent(imageBasedLight: earth))
-                     earth.components.set(GroundingShadowComponent(castsShadow: true))
-                     
-                     earth.components[PhysicsBodyComponent.self] = .init(PhysicsBodyComponent(
-                         massProperties: .default,
-                         material: .generate( staticFriction: 0.8, dynamicFriction:0.5, restitution: 0.1),
-                         mode: .dynamic
-                     ))
-                     
-                     earth.components[PhysicsMotionComponent.self] = .init()
-                     
-                     content.add(earthModel)
-             }
+                if let moonModel =   buildModel(modelName: "moon", scaleValue: 0.1){
+                    earthModel.addChild(moonModel)
+                    
+                    let radians = 90.0 * Float.pi / 180.0
+                    
+                    moonModel.transform.translation += SIMD3<Float>(0.0, 1.0, 0.0)
+                    moonModel.orientation = simd_quatf(angle: radians, axis: SIMD3(x: 0, y: 1, z: 0))
+                    moonModel.transform.scale *= 0.5
+                    
+                    content.add(moonModel)
+                }
+                content.add(earthModel)
+            }
     
         }
-        .gesture(dragGesture)
+//        .gesture(dragGesture)
     }
     
     var dragGesture: some Gesture{
@@ -63,8 +51,37 @@ struct ImmersiveView: View {
                 }
             
     }
-      
+    
+    private func buildModel(modelName: String, scaleValue: Float)  -> Entity? {
         
+        guard let modelEntity = try? Entity.load(named: modelName) else {
+                print("Failed to load model named \(modelName)")
+                return nil
+            }
+        if let modelIdentity =  modelEntity.children.first?.children.first{
+            modelIdentity.position.y = 0.5
+            
+            modelIdentity.scale = [scaleValue, scaleValue, scaleValue]
+            
+            // Generate the collision effect with the base
+            modelIdentity.generateCollisionShapes(recursive: true)
+            modelIdentity.components.set(InputTargetComponent(allowedInputTypes: .all))
+//            modelIdentity.components.set(ImageBasedLightComponent(source: .single(environment)))
+//            modelIdentity.components.set(ImageBasedLightReceiverComponent(imageBasedLight: modelIdentity))
+//            modelIdentity.components.set(GroundingShadowComponent(castsShadow: true))
+            
+            modelIdentity.components[PhysicsBodyComponent.self] = .init(PhysicsBodyComponent(
+                massProperties: .default,
+                material: .generate( staticFriction: 0.8, dynamicFriction:0.5, restitution: 0.1),
+                mode: .dynamic
+            ))
+            
+            modelIdentity.components[PhysicsMotionComponent.self] = .init()
+        }
+                 
+        return modelEntity
+        
+         }
     
     
     private func createSkyBox() -> Entity? {
@@ -72,7 +89,7 @@ struct ImmersiveView: View {
         var skyboxMaterial =  UnlitMaterial()
         
         do{
-            let texture =  try TextureResource.load(named: "demo")
+            let texture =  try TextureResource.load(named: "starmap_2020_4k")
             skyboxMaterial.color = .init(texture: .init(texture))
         } catch {
             print("Failed to create skybox material: \(error)")
@@ -94,3 +111,4 @@ struct ImmersiveView: View {
     ImmersiveView()
         .previewLayout(.sizeThatFits)
 }
+
